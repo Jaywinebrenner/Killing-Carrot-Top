c3a85ad13e5902078bb35355f0b77d1078c7979e
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-01-04 15:15:19 -0800

    add further locations

--- game.rb
+++ game.rb
@@ -3,10 +3,7 @@ require_relative "lib/enemy.rb"
 require_relative "lib/location.rb"
 # require_relative "lib/locatins/location.data.rb"
 require_relative "lib/battle.rb"
-require_relative 'lib/intro.rb'
-
-
-
+# require_relative 'lib/intro.rb'
 
 # gallagher.attack(player)
 # player.attack(gallagher)
@@ -22,28 +19,56 @@ class Game
     end
 
     def create_player
-      create_locations
+      puts ""
+      puts " __   .__.__  .__  .__                      "
+      puts "|  | _|__|  | |  | |__| ____    ____        "
+      puts "|  |/ /  |  | |  | |  |/    \  / ___\       "
+      puts "|    <|  |  |_|  |_|  |   |  \/ /_/  >      "
+      puts "|__|_ \__|____/____/__|___|  /\___  /       "
+      puts "     \/                    \//_____/        "
+      puts "_________                             __    "
+      puts "\_   ___ \_____ ______________  _____/  |_  "
+      puts "/    \  \/\__  \\_  __ \_  __ \/  _ \   __\ "
+      puts "\     \____/ __ \|  | \/|  | \(  <_> )  |   "
+      puts " \______  (____  /__|   |__|   \____/|__|   "
+      puts "        \/     \/                           "
+      puts "___________                                 "
+      puts "\__    ___/___ ______                       "
+      puts "  |    | /  _ \\____ \                      "
+      puts "  |    |(  <_> )  |_> >                     "
+      puts "  |____| \____/|   __/                      "
+      puts "               |__|                        "
+      puts ""
+      puts ""
+      puts "Spawned from the fetid and pestulent comedy clubs from the dark edges of civilization, you embark on your journey to exact revenge upon this foul world from which you suffered so."
+      puts ""
+      puts "Your singular journey is to infiltrate the elite comedy circuit of the Illuimiati of Laughter- a sinisiter coalition of comedians hell bent on the enslavement of the human race, world dominance and crushing the dignity of the human spirit."
+      puts ""
+      puts "You must assassinate the Ultimate Comedy Beast, Lord of Laughter, King of the Cutups, the Baron of Belly Busting himself - Carrot Top. It will be a perilous journey filled with violence, mayhem, depravity and suffering."
+      puts ""
       puts ""
       puts "What is thy name, Comedian Slayer?"
       puts ""
       user_input_name = gets.chomp
       puts ""
-      puts "Before you embark on your journey, you must pick one of three weapons at thy disposal."
+      puts user_input_name + " ... Tis a fantastic name. Before you embark on your perilous journey, you must pick one of three weapons at your disposal."
       puts ""
-      # puts "And there are 3 weapons at thy disposal as you sink deep within this quagmire of comedy. Which do you choose?"
       puts "1 - A butter knife"
       puts "2 - A broken 2x4"
       puts "3 - A trash can lid"
+      puts "4 - Take nothing"
       weapon = gets.chomp
       if weapon == "1"
         puts "You admire the sheen on the butter knife and lick the edges adoringly."
-        @player.damage + 2
+        @player.damage = @player.damage + 3
       elsif weapon == "2"
         puts "You examine the 2x4 and lick your lips."
-        @player.damage + 2
+        @player.damage = @player.damage + 4
       elsif weapon == "3"
         puts "As you pick up the trash lid, you catch your refelction in the sheen of the metal. You are a broken soul. Perhaps this trash can lid will help mend it."
-        @player.damage + 1
+        @player.damage = @player.damage + 1
+      elsif weapon == "4"
+        puts "You didn't grab a weapon and simply opt to battle the Illuimiati of Laughter with your fists."
       else
         puts "You didn't grab a weapon and simply opt to battle the Illuimiati of Laughter with your fists."
       end
@@ -52,11 +77,11 @@ class Game
       puts "We wish you the best of luck, " + user_input_name + " as you sink deep within this vile quagmire of comedy."
       puts ""
       @player.name = user_input_name
-
+      create_locations
     end
 
     def create_locations
-      Location.new(nil).intro
+      Location.new(nil).alley
         #for i in loop while whater:
         # Location.new(add enemies if you want)
         # then youll have x number of locaitons
@@ -69,10 +94,11 @@ class Game
 end
 
 # newGame = Intro.new
+emo_phillips = Enemy.new("Emo Phillips", 10, 3)
 beginingLocation = Location.new(nil)
 game = Game.new.initialize
-battle = Battle.new
+# battle = Battle.new
 
-beginingLocation.tim_allens_spectre
-player = Player.new("Dangle", 20, 5)
-gallagher = Enemy.new("Gallagher", 10, 2)
+# beginingLocation.tim_allens_spectre
+# player = Player.new("Dangle", 20, 5)
+# gallagher = Enemy.new("Gallagher", 10, 2)
--- lib/location.rb
+++ lib/location.rb
@@ -3,41 +3,45 @@ class Location
     @enemies = enemies
   end
 
-  def intro
+  def alley
+    puts "You have traversed the sprawling metropolis of Laughlandia for weeks on end and have successfully spotted the elusive Prop Tower where the inner sanctum of the Illuminati of Laughter resides. Slinking through garbage strewn alley ways, skillfully avoiding the army of Cybernetic Dane Cooks on partrol, you are greeted with the unusual smell of sauteed garlic clearly eminating from a ramshackled tent put together via rusted shopping carts and shredded blue tarps."
     puts ""
-    puts " __   .__.__  .__  .__                      "
-    puts "|  | _|__|  | |  | |__| ____    ____        "
-    puts "|  |/ /  |  | |  | |  |/    \  / ___\       "
-    puts "|    <|  |  |_|  |_|  |   |  \/ /_/  >      "
-    puts "|__|_ \__|____/____/__|___|  /\___  /       "
-    puts "     \/                    \//_____/        "
-    puts "_________                             __    "
-    puts "\_   ___ \_____ ______________  _____/  |_  "
-    puts "/    \  \/\__  \\_  __ \_  __ \/  _ \   __\ "
-    puts "\     \____/ __ \|  | \/|  | \(  <_> )  |   "
-    puts " \______  (____  /__|   |__|   \____/|__|   "
-    puts "        \/     \/                           "
-    puts "___________                                 "
-    puts "\__    ___/___ ______                       "
-    puts "  |    | /  _ \\____ \                      "
-    puts "  |    |(  <_> )  |_> >                     "
-    puts "  |____| \____/|   __/                      "
-    puts "               |__|                        "
+    puts "Do you: "
     puts ""
+    puts "1 - Continue towards Prop Tower"
+    puts "2 - Carefully approach the tent and sumptuous smells"
+    puts "3 - Hide behind a nearby dumpster to survey the tent"
     puts ""
-    puts "Spawned from the fetid and pestulent comedy clubs from the dark edges of civilization, you embark on your journey to exact revenge upon this foul world from which you suffered so."
-    puts ""
-    puts "Your singular journey is to infiltrate the elite comedy circuit of the Illuimiati of Laughter- a sinisiter coalition of comedians hell bent on the enslavement of the human race, world dominance and crushing the dignity of the human spirit."
-    puts ""
-    puts "You must assassinate the Ultimate Comedy Beast, Lord of Laughter, King of the Cutups, the Baron of Belly Busting himself - Carrot Top. It will be a perilous journey filled with violence, mayhem, depravity and suffering."
-    puts ""
+    answer = gets.chomp
+    if answer == "1"
+      puts "You deem it unwise to investigate the lovely smells despite your rumbling stomach. You continue towards your desintation."
+      tim_allens_spectre()
+    elsif answer == "2"
+      puts "As you approach the alluring smells, a lanky figure cloaked in shadow emerges from the make-shift tent. 'How dare you', says an eerie, child-like voice crackling in-and-out of falsetto. 'How dare you intruupt my sauteed human hand buffet.' The figure steps into the moonlight and tosses a roasted human hand at your feet. 'No one intruupts Emo Phillips as he dines!' He pulls up his lanky arm, points at you and howls in a terrifying ear-piercing shriek."
+      # battle()  how call battle
+    elsif answer == "3"
+      puts "You punch yourself in the face. A true Comedy Slayer must never rest on the laurels of indecision. In order to take down the Illuminati of Laughter, one must never sit idle."
+      puts ""
+      puts "You take 2 Hit Points of damage."
+      alley()
+      # @player.health -= 2 how to affect player
+    else
+      puts "You punch yourself in the face twice. A true Comedy Slayer must never rest on the laurels of indecision. In order to take down the Illuminati of Laughter, one must never sit idle."
+      puts ""
+      puts "You take 2 Hit Points of damage."
+      alley()
+      # @player.health -= 3 how to affect player
+    end
+
+
   end
 
   def tim_allens_spectre
-    puts "You have traversed the sprawling metropolis of Laughlandia for weeks on end and have successfully found the elusive Prop Tower where the inner sanctum of the Illuminati of Laughter resides."
+    puts "Crawling through a large pile of municipal waste and rubbish, you emerge in the courtyard of the elusive Prop Tower."
     puts ""
     puts "It is an imposing structure with life-like statues of blood thristy comedians looming from the tresses like maniacal gargoyles."
-    puts "As you approach, the bellowing voice of Tim Allen echos throughout the street. 'Welcome Comedian Slayer. To gain access to Prop Tower, you must answer 3 questions about my celebrated situational comedy Home Improvement.'"
+    puts ""
+    puts "As you approach, the bellowing voice of Tim Allen echos throughout the coutryard. 'Welcome Comedian Slayer. To gain access to Prop Tower, you must answer 3 questions about my celebrated situational comedy Home Improvement.'"
     puts ""
     puts "'What was the name of the revered child actor who portrayed my son Randy?'"
     answer_one = gets.chomp
@@ -46,6 +50,7 @@ class Location
     else
       damage = rand(1..4)
       puts "'WRONG!' bellows the disembodied voice of Tim Allen as several hissing measuring tapes materialize out of thin air and whip you in the face for #{damage} hit points of damage."
+      @player.health -= damage
     end
     puts ""
     puts "'What was the name of my characters ficticious television show?'"
@@ -67,16 +72,32 @@ class Location
     end
     puts "And thus you have passed my test. You may proceed into Prop Tower."
     puts ""
-    puts "The iron wrought doors afixed with an ornate bas-relief depiction of Jimmy Durante creek open with the distant eerie sounds of 'Ha cha cha cha'"
+    puts "The iron wrought doors afixed with an ornate bas-relief depiction of Jimmy Durante creek open accompanied with the distant eerie sounds of 'Ha cha cha cha'"
     puts ""
-    foyer()
+    movement = gets.chomp
+    puts "Do you: "
+    puts "1 - Continue through the iron wrought door into the belly of the Illuminati of Laughter"
+    puts "2 -Traverse the outsides of the tower for an alternate entrance."
+    if movement == "1"
+      foyer()
+    elsif movement == "2"
+      puts "You walk the garbage strewn perimeter of the building in search of a less obvious entrance all for naught. As you circle back towards the iron wrought front doors, the familiar voice of Tim Allen can be heard bellowing from above. 'How dare you insult me by not accepting my graciousness! Feel the wrath of my Makita Circular saw!' Tim Allen's statue flips from Prop Tower's tresses in impressive Olympic fashion, lands several yards away and attacks."
+      # battle()
+    else
+      puts "You punch yourself in the face. A true Comedy Slayer must never rest on the laurels of indecision. In order to take down the Illuminati of Laughter, one must never sit idle."
+      puts ""
+      puts "You take 2 Hit Points of damage."
+      # @player.health -= 2
+    end
   end
 
   def foyer
-    puts "You enter the vast foyer of Prop tower. The room is encapsulated in shadow and pain. You can feel the thick black blood of depravity hanging in the air. This is truely an unholy, otherworldly and vile place. You winch at the stench of sulfer and human waste. There is a large staircase which goes up to a landing containing three doors. Door number one is strangely ornate with various flower species carved into it's heavy wooden frame. The soft and unsettling sounds of what seems to be an accordian can be heard it the distance. Door Number two is crudely painted in an unsettling prison strip pattern and looks to have rotting watermelon rinds strewn about it's base. Door number three is smeared with a sticky tar like substance which reeks of diesel gasoline and casual bigotry."
+    puts "You enter the vast foyer of Prop tower. The room is encapsulated in shadow and pain. You can feel the thick black blood of depravity hanging in the air. This is truely an unholy, otherworldly and vile place. You winch at the stench of sulfer and human waste. There is a large staircase which goes up to a landing containing three doors. Door number one is strangely ornate with various flower species carved into it's heavy wooden frame. The soft and unsettling sounds of what seems to be an accordian can be heard in the distance. Door Number two is crudely painted in an unsettling prison strip pattern and looks to have rotting watermelon rinds strewn about it's base. Door number three is smeared with a sticky tar like substance which reeks of diesel gasoline and casual bigotry."
     puts ""
     puts "Which door do you enter?"
-    puts "1, 2 or 3?"
+    puts "1"
+    puts "2"
+    puts "3"
     puts ""
     door_choice = gets.chomp
     if door_choice == "1"
@@ -92,18 +113,28 @@ class Location
   end
 
   def judy_tenutas_coven
-    puts "You gingerly make your way through a long windowless hallway as the sounds of what appears to be an accordian competently played gets louder and louder. As you approach an archway which leads to an open room shrouded in darkness, the sound abruptly stops. An upright sarcophagus stand perfectly in the room's center with bones strewn about it's base. The sudden void the accordian's syllabic tones leaves you uneasy. You get the feeling you are being watched."
+    puts "You gingerly make your way through a long windowless hallway as the sounds of what appear to be an accordian competently played gets louder and louder. As you approach an archway which leads to an open room shrouded in darkness, the sound abruptly stops. An upright sarcophagus stand perfectly in the room's center with bones strewn about it's base. The sudden void the accordian's syllabic tones leaves you uneasy. You get the feeling you are being watched."
     puts "Do you: "
-    puts "1- Move forward to investigate the sarcophagus or "
-    puts "2 - Retreat back into the lobby."
+    puts "1- Move forward to investigate the sarcophagus"
+    puts "2 - Retreat back into the lobby"
+    puts "3 - Stealthily sneak past the sarcophagus and attempt slip into a hallway on the other side of the room"
     answer = gets.chomp
     if answer == "1"
-      puts "As you make your way towards the sarcophagus, an ear piercing shreik knifes your ears and the ominious bellows blast from an accordian. 'Helllllo, Comedian Slayer!' mocks a voice in the unmistakable affectation of famed 80s character comedian Judy Tenuta.
+      puts "As you make your way towards the sarcophagus, an ear piercing shreik knifes your ears and and ominious blast bellows from an accordian. 'Helllllo, Comedian Slayer!' mocks a voice in the unmistakable affectation of famed 80s character comedian Judy Tenuta.
       'Welcome to the Illuminati of Laughter, where you will die horribly soundtracked by my accordian tones!' Judy Tenuta blasts out of a pile of bones and hovers in the air. She omits a hysterical cackle and attacks!"
+      # battle()
     elsif answer == "2"
       foyer()
+    elsif answer == "3"
+      puts "As you make your around the sarcophagus, an ear piercing shreik knifes your ears and an ominious blast bellows from an accordian. 'Helllllo, Comedian Slayer!' mocks a voice in the unmistakable affectation of famed 80s character comedian Judy Tenuta.
+      'Welcome to the Illuminati of Laughter, where you will die horribly soundtracked by my accordian tones!' Judy Tenuta blasts out of a pile of bones and hovers in the air. She emits a hysterical cackle and attacks!"
+        # battle()
     else
-      puts "Please enter 1 to investigate the sarcophagus or 2 to go back to the foyer."
+      puts "You punch yourself in the face. A true Comedy Slayer must never rest on the laurels of indecision. In order to take down the Illuminati of Laughter, one must never sit idle."
+      puts ""
+      puts "You take 2 Hit Points of damage."
+    # @player.health -= 2
+      judy_tenutas_coven()
     end
 
   end
